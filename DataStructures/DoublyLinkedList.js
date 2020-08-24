8c543838574dbfdc7ed40917649d6a5a6e55c1a9
Sushi <susheendharvj@gmail.com>
2020-08-24 21:34:48 +0530

    finished DLL

--- DataStructures/DoublyLinkedList.js
+++ DataStructures/DoublyLinkedList.js
@@ -50,15 +50,99 @@ class DoublyLinkedList {
       this.head = null;
       this.tail = null;
       this.length--;
-      return oldhead;
+      return oldHead;
     }
     if (this.length === 0) return undefined;
 
     this.head = oldHead.next;
     this.head.prev = null;
-    oldhead.next = null;
+    oldHead.next = null;
     this.length--;
-    return oldhead;
+    return oldHead;
+  }
+
+  unshift(value) {
+    let newNode = new Node(value);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+      this.length++;
+      return this;
+    }
+    newNode.next = this.head;
+    this.head.prev = newNode;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+  get(index) {
+    let currentNode, counter;
+    if (index >= this.length || index < 0) {
+      return null;
+    }
+    let middle = Math.floor(this.length / 2);
+    if (index <= middle) {
+      counter = 0;
+      currentNode = this.head;
+      while (counter !== index) {
+        currentNode = currentNode.next;
+        counter++;
+      }
+      return currentNode;
+    } else {
+      counter = this.length - 1 - index;
+      currentNode = this.tail;
+      while (counter !== 0) {
+        currentNode = currentNode.prev;
+        counter--;
+      }
+      return currentNode;
+    }
+  }
+  set(index, value) {
+    let node = get(index);
+    if (node) {
+      node.val = value;
+      return true;
+    }
+    return false;
+  }
+  insert(index, value) {
+    if (index === 0) return !!this.unshift(value);
+
+    if (index === this.length) return !!this.push(value);
+
+    let newNode = new Node(value);
+    let prevNode = this.get(index - 1);
+    if (prevNode) {
+      newNode.next = prevNode.next;
+      prevNode.next = newNode;
+      newNode.next.prev = newNode;
+      newNode.prev = prevNode;
+      this.length++;
+      return true;
+    }
+    return false;
+  }
+
+  remove(index) {
+    if (index === 0) return this.unshift();
+
+    if (index === this.length - 1) return this.pop();
+
+    let prevNode = this.get(index - 1);
+    if (prevNode) {
+      let removedNode = prevNode.next;
+
+      prevNode.next = removedNode.next;
+      prevNode.next.prev = prevNode;
+
+      removedNode.next = null;
+      removedNode.prev = null;
+      this.length--;
+      return removedNode;
+    }
+    return undefined;
   }
 }
 
