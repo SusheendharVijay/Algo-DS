e20614c1cc194f552e2fb7f97f8a35d219bebdc9
Sushi <susheendharvj@gmail.com>
2020-09-01 17:50:46 +0530

    finished dfs for graphs

--- DataStructures/Graph.js
+++ DataStructures/Graph.js
@@ -6,7 +6,7 @@ class Graph {
     if (!this.adjacencyList) this.adjacencyList[vertex] = [];
   }
   addEdge(vertex1, vertex2) {
-    // can add error hadnling here like checking for valid vertex etc.
+    // can add error handling here like checking for valid vertex etc.
     this.adjacencyList[vertex1].push(vertex2);
     this.adjacencyList[vertex2].push(vertex1);
   }
@@ -23,5 +23,35 @@ class Graph {
       this.removeEdge(vertex, v);
     }
     delete this.adjacencyList[vertex];
+  }
+  dfsRecursive(vertex) {
+    let traversal = [];
+    let visited = {};
+    function helperFunction(vertex) {
+      if (!vertex) return;
+      traversal.push(vertex);
+      visited[vertex] = true;
+      for (neighbor of this.adjacencyList[vertex]) {
+        if (!visited[neighbor]) helperFunction(neighbor);
+      }
+    }
+    helperFunction(vertex);
+    return traversal;
+  }
+  dfsIterative(vertex) {
+    let stack = [vertex];
+    let visited = {};
+    let traversal = [];
+    let current;
+    while (stack.length) {
+      current = stack.pop();
+      traversal.push(current);
+      visited[current] = true;
+      for (let neighbor of this.adjacencyList[current]) {
+        if (!visited[neighbor] && !stack.includes(neighbor))
+          stack.push(neighbor);
+      }
+    }
+    return traversal;
   }
 }
